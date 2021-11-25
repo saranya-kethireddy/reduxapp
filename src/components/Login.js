c0c45495f80cab0a2c58e0b0d9caff9efdbb9422
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-25 15:48:00 +0530

    perform logout function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useDispatch } from "react-redux";
+import{ login ,logout} from "../features/user";
+
 
 function Login() {
+
+    const dispatch = useDispatch();
     return (
         <div>
-            <button>Login</button>
+            <button onClick = {() => {
+                dispatch(Login({name: "saranya", age: 22, email: "sar@gmail.com"}));
+            }}
+                  >
+                      Login
+                        </button>
+
+                        <button
+                        onClick={() => {
+                            dispatch(logout({name: "saranya", age: 22, email: "sar@gmail.com"}));
+                        }}
+                        >
+                            LOGOUT
+                        </button>
         </div>
     )
 }
